d973acf60d0e6d445cfa97ae57f968098193343b
C-hetha-N <chethannchethann80@gmail.com>
2024-05-08 19:02:16 +0530

    Lecture 57 completed - Loops

---
+++ JavaScript/Lecture 57/loops.js
@@ -0,0 +1,34 @@
+console.log("I am a tutorial on Loops");
+
+// let a = 1;
+
+// for(let i=0; i<10; i++) {
+//     console.log(a+i);
+// }
+
+// let obj = {
+//     name: "chethan",
+//     course: "CSE",
+//     college: "BMSCE"
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, element);
+// }
+
+// for (const iterator of "Hello World!") {
+//     console.log(iterator);
+// }
+
+// let j = 1;
+// while (j < 6) {
+//     console.log(j);
+//     j++;
+// }
+
+let j = 10;
+do {
+    console.log(j);
+    j++;
+} while (j < 6);
