5fd65269af5082c56302dc258ad7ab082a188965
C-hetha-N <chethannchethann80@gmail.com>
2024-05-10 12:10:58 +0530

    Lecture 60 Strings - Completed

---
+++ JavaScript/Lecture 60/string.js
@@ -0,0 +1,48 @@
+console.log("This is a String Tutorial");
+
+// let a = "Str";
+// console.log(typeof(a));
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+
+// console.log(a.length);
+
+let my_name = "Chethan";
+let frd = "Rocky";
+
+console.log("His name is " + my_name + " and his friend name is " + frd);
+console.log(`His name is ${my_name} and his friend name is ${frd}`)  
+// ^ Templete literals and varibles are String interpolation
+
+console.log("\nRohan");
+console.log("Roh'an");
+// console.log("Roh"an");    
+console.log(`Roh"an`);   
+console.log("Roh\"an");   
+console.log(`Roh\`an`);
+
+let men = "Shiva Sundra";
+console.log("\n" + men);
+console.log(men.length);
+console.log(men.toLowerCase());
+console.log(men.toUpperCase());
+console.log(men.slice(1,4));
+console.log(men.slice(1));
+console.log(men.slice(1,4,2));
+
+console.log(men.replace("Sundra","Kumar"));
+console.log(men.replace("a",""));
+
+console.log(men.concat(" Satya ",my_name));
+console.log();
+
+let x = "   Star Boy   ";
+console.log(x);
+console.log(x.trim());
+
+console.log("\n" + men);
+//Immutable
+
+
