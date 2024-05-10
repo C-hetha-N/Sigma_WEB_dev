5bc552ffc5a93bc6f1d7fc12eb0b442eec6ffb94
C-hetha-N <chethannchethann80@gmail.com>
2024-05-10 12:12:58 +0530

    Lecture 62 Ex10 - Challenge Accepted

---
+++ JavaScript/Lecture 62 Ex10/nameGen.js
@@ -0,0 +1,18 @@
+/*
+    Create a buisness name generator by combining list of abjectives and shop name and another word
+
+    Adjectives:
+    Crazy
+    Amazing
+    Fire
+
+    Shop Name:
+    Engine
+    Foods
+    Garments
+
+    Another word:
+    Bros
+    Limited 
+    Hub
+*/
