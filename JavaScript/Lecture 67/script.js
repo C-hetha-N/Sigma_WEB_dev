8f5f0090713ab1a05ad4c8506633eb4e5f259424
C-hetha-N <chethannchethann80@gmail.com>
2024-05-12 09:21:30 +0530

    L67 updated

--- JavaScript/Lecture 67/script.js
+++ JavaScript/Lecture 67/script.js
@@ -1,1 +1,18 @@
 console.log("Hello World!");
+
+// document.body
+// document.body.childElementCount
+// document.body.childNodes
+// document.body.children
+// document.body.childNodes[1].childNodes
+// document.body.firstChild
+// document.body.firstElementChild
+// document.body.firstElementChild.children[1].nextElementSibling
+// document.body.firstElementChild.parentElement
+
+// table links
+
+// tbl = document.body.children[1]
+// tbl.rows
+// tbl.caption
+// tbl.tHead
