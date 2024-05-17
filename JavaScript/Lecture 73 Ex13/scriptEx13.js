ae6525f962fe5908a2e6b272627449e19b0726a4
C-hetha-N <chethannchethann80@gmail.com>
2024-05-17 11:19:21 +0530

    Lecture 73 Ex13 - Challenge Accepted

---
+++ JavaScript/Lecture 73 Ex13/scriptEx13.js
@@ -0,0 +1,5 @@
+function cardMaking(title, cName, views, monthsOld, duration, thumbnail) {
+    // Finish this function
+}
+
+cardMaking("Introduction to Backend | Sigma web development #2", "codewithharry", 7640000, 2, "28.35", imgThumbnail); 
