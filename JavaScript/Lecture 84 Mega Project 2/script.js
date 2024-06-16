37cebdfb51dbf95f4f9c11baffbab71370707b05
C-hetha-N <chethannchethann80@gmail.com>
2024-06-16 10:16:38 +0530

    Playbar created

--- JavaScript/Lecture 84 Mega Project 2/script.js
+++ JavaScript/Lecture 84 Mega Project 2/script.js
@@ -1,5 +1,27 @@
 console.log("Let's write JavaScript");
 
+let currentSong = new Audio();
+
+function formatTime(seconds) {
+    // Ensure the input is an integer by flooring it
+    const totalSeconds = Math.floor(seconds);
+
+    // Calculate hours, minutes, and remaining seconds
+    const hours = Math.floor(totalSeconds / 3600);
+    const remainingMinutes = Math.floor((totalSeconds % 3600) / 60);
+    const remainingSeconds = totalSeconds % 60;
+
+    // Pad with leading zeros if necessary
+    const formattedHours = String(hours).padStart(2, '0');
+    const formattedMinutes = String(remainingMinutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    // Combine hours, minutes, and seconds into the desired format
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
+
+
+
 async function getSongs() {
     let a = await fetch("http://127.0.0.1:3000/songs/")
     let response = await a.text()
@@ -17,11 +39,23 @@ async function getSongs() {
     return songs;
 }
 
+const playMusic = (track, pause=false) => {
+    currentSong.src = "/songs/" + track
+    if(!pause) {
+        currentSong.play()
+        play.src = "pause.svg"
+    }
+    document.querySelector(".songName").innerHTML = decodeURI(track)
+    document.querySelector(".songTime").innerHTML = "00:00 / 00:00"
+}
+
 async function main() {
+
     // Get the list of all songs
     let songs = await getSongs()
-    console.log(songs);
+    playMusic(songs[0],true)
 
+    // Show all songs in the playlist
     let songUL = document.querySelector(".songsList").getElementsByTagName("ul")[0]
     for (const song of songs) {
         songUL.innerHTML = songUL.innerHTML + `<li>
@@ -34,19 +68,42 @@ async function main() {
                             </div>
                             <div class="playNow">
                                 <span>Play Now</span>
-                                <img src="playIcon.svg" alt="">
+                                <img src="play.svg" alt="">
                             </div>
                         </li>`
     }
-
-    // Play the first song
-    var audio = new Audio(songs[0]);
-    audio.play();
-
-    audio.addEventListener("loadeddata", () => {
-        let duration = audio.duration
-        console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    Array.from(document.querySelector(".songsList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            playMusic(e.querySelector(".songInfo").getElementsByTagName("div")[1].innerHTML.trim());
+        })
     })
+
+
+    // Attach an event listener to play, next or previous
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
+            currentSong.play()
+            play.src = "pause.svg"
+        }
+        else {
+            currentSong.pause()
+            play.src = "play.svg"
+        }
+    })
+
+    // Listen for timeUpdate event
+    currentSong.addEventListener("timeupdate", ()=>{
+        document.querySelector(".songTime").innerHTML = `${formatTime(currentSong.currentTime)} / ${formatTime(currentSong.duration)}`
+        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%"
+    })
+
+    // Add event listener to seekbar
+    document.querySelector(".seekbar").addEventListener("click", e=> {
+        let seekbarPercent = e.offsetX / e.target.getBoundingClientRect().width * 100
+        document.querySelector(".circle").style.left = seekbarPercent + "%"
+        currentSong.currentTime = (currentSong.duration) * seekbarPercent / 100
+    })
+    
 }
 
 main()
